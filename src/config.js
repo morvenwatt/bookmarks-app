c2845bac4dbbc5e56ca0914256578934c44ee003
Morven Watt <morvenwatt@gmail.com>
2019-10-16 14:20:54 -0700

    refactored prior to context lessons

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
+  API_KEY: '$2a$10$Y9N2kClCQGpWF57F3jZmUOnjlcGSL0cWsbbDrw50.pcQCniNe.V3O',
 }
